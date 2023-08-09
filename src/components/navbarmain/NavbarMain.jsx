98907ff69ecd06a1056ebacba7e44a404e610d73
Ramón Serrano <ramonserrano76@gmail.com>
2023-08-08 22:54:42 -0300

    commit 7

--- src/components/navbarmain/NavbarMain.jsx
+++ src/components/navbarmain/NavbarMain.jsx
@@ -21,16 +21,12 @@ const NavbarMain = () => {
                         <Button variant="outline-light">Search</Button>
                     </Form> */}
                     {/* Opciones de navegación */}
-                    <Nav className="ms-auto my-1 my-lg-0 justify-content-center">
-                        <Nav.Link as={Link} to="/home" className="text-white">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/cities" className="text-white">Cities</Nav.Link>
-                        <Nav.Link href="#login" className="btn btn-primary text-white">Login{' '}
-                            <div className="user-image d-inline">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                    <Nav className="ms-auto my-1 my-lg-0 justify-content-center align-itens.center">
+                        <Nav.Link as={Link} to="/home" className="btn justify-content-center">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/cities" className="btn justify-content-center">Cities</Nav.Link>
+                        <Nav.Link href="#login" className="btn btn-primary">Login{' '}<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm0 3a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm0 7.5c1.77 0 3.267.978 4.088 2.416-.025 1.135-2.152 1.834-4.088 1.834s-4.063-.699-4.088-1.834C4.733 11.478 6.23 10.5 8 10.5zM8 2c-.697 0-1.359.116-1.98.324a5.984 5.984 0 0 1 3.96 0C9.36 2.116 8.698 2 8 2z" />
-                                </svg>
-                            </div>
-                        </Nav.Link>
+                                </svg></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,3 @@
-// // src/pages/Home.jsx
-// import React from 'react';
-// import './home.css';
-// import Carrousel from '../../components/carrousel/Carrousel.jsx';
-// import { Link } from 'react-router-dom';
-
-// const Home = () => {
-//     return (
-//         <div className="home-container">
-//             <div className="hero">
-//                 <div className="hero-image-container">
-//                     <h1>MyTinerary</h1>
-//                     <p>Find your perfect trip, designed by insiders who know and love their cities!</p>
-//                     <Link to="/cities" className="btn btn-secondary">Explore Cities</Link>
-//                 </div> 
-//                 <Carrousel />
-//             </div>
-        
-//         </div>
-//     );
-// };
-
-// export default Home;
-
-
 import React, { useState, useEffect } from 'react';
 import './home.css';
 import Carrousel from '../../components/carrousel/Carrousel.jsx';
